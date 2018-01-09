f5444c7890bacb9368c8be87c92441bedd865f31
Rbatts <32482377+Rbatts@users.noreply.github.com>
2018-01-09 09:53:51 +0000

    Json added, search transactions allowed

--- ConsoleApp8/supportbank.cs
+++ ConsoleApp8/supportbank.cs
@@ -5,8 +5,13 @@ using NLog.Targets;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
+using System.Threading;
 using System.Text;
 using System.Threading.Tasks;
+using NLog.LayoutRenderers.Wrappers;
+using System.Xml;
+using System.IO;
 
 namespace supportbank
 {
@@ -15,25 +20,28 @@ namespace supportbank
         private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
 
         public static void Main(string[] args)
-
         {
             var config = new LoggingConfiguration();
             var target = new FileTarget { FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
             config.AddTarget("File Logger", target);
             config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
             LogManager.Configuration = config;
+            
             logger.Log(LogLevel.Info, "Program starting");
 
             string path = @"C:\Users\Rich\test\supportproject\Transactions2014.csv";
             string textFromFile = System.IO.File.ReadAllText(path);
             logger.Log(LogLevel.Info, "2014 transaction Files uploaded");
+            textFromFile = textFromFile.ToLower();
 
             string path2 = @"C: \Users\Rich\Downloads\dodgy transactions.csv";
             string textFromFile2 = System.IO.File.ReadAllText(path2);
             logger.Log(LogLevel.Info, "Dodgy Files uploaded");
+            textFromFile2 = textFromFile2.ToLower();
 
             string path3 = @"C: \Users\Rich\Downloads\Transactions2013.json";
             string textFromFile3 = System.IO.File.ReadAllText(path3);
+            textFromFile3 = textFromFile3.ToLower();
             var transactions = JsonConvert.DeserializeObject<List<Transaction>>(textFromFile3);
             List<Person> employees = new List<Person>();
             foreach (var transaction in transactions)
@@ -85,7 +93,7 @@ namespace supportbank
                 {
                     logger.Log(LogLevel.Error, "Error with a line of data file");
                     logger.Log(LogLevel.Error, lines[i]);
-                    Console.WriteLine("Error detected. Please check the log file");
+                    Console.WriteLine("\nError detected. Please check the log file");
                 }
             }
 
